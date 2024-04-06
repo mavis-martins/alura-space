96676ac001695045fbcad7751d28f51edf6f2673
mavis-martins <mavismr98@gmail.com>
2024-04-05 22:42:59 -0300

    criação dos formulários de cadastro e login com django.forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroForm
 
 # Create your views here.
 def login(request):
-    return render(request, 'usuarios/login.html')
+
+    form = LoginForms()
+
+    return render(request, 'usuarios/login.html', {'form': form})
+
+def cadastro(request):
+
+    form = CadastroForm()
+
+    return render(request, 'usuarios/cadastro.html', {'form': form})
